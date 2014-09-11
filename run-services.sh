3353c47b3949fde806f35504152c8855ca04f9a7
Gabriel Melillo <gabriel@melillo.me>
2014-09-11 12:34:52 +0200

    Removed trusted_domain check every trusted_domain related error will be managed over ssh connection

--- run-services.sh
+++ run-services.sh
@@ -2,8 +2,6 @@
 if [ ! -f /.root_pw_set ]; then
 	/set_root_pw.sh
 fi
-php5 /usr/local/bin/remove_trusted_domains.php > /usr/share/nginx/owncloud/config/config.php
-chown www-data:www-data -R /usr/share/nginx/owncloud/config/ 2> /vev/null
 service php5-fpm start 
 service mysql start 
 service nginx start
