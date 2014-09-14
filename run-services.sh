0c8d21ee2dbf6fda95c086e09c40949085592793
Gabriel Melillo <gabriel@melillo.me>
2014-09-14 21:51:08 +0200

    changed folder to check to evaluate if database is installed correctly

--- run-services.sh
+++ run-services.sh
@@ -3,7 +3,7 @@ if [ ! -f /.root_pw_set ]; then
 	/set_root_pw.sh
 fi
 
-if [ -z "$(ls -A /var/lib/mysql)" -a "${1%_safe}" = 'mysqld' ]; then
+if [ -z "$(ls -A /var/lib/mysql/mysql/)" -a "${1%_safe}" = 'mysqld' ]; then
     rm -rf /var/lib/mysql/*
     chown -R mysql /var/lib/mysql && chgrp -R mysql /var/lib/mysql
     mysql_install_db
