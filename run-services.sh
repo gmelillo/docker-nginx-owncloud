f0c35792cf56c4d2f72a46ef141729912eaab79d
Gabriel Melillo <gabriel@melillo.me>
2014-09-14 23:59:16 +0200

    Fixed error on database initialization

--- run-services.sh
+++ run-services.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
+chmod +x /usr/local/bin/*.sh
+PURGE_EXPECT_WHEN_DONE=1
+
 if [ ! -f /.root_pw_set ]; then
 	/set_root_pw.sh
 fi
 
-if [ -z "$(ls -A /var/lib/mysql/mysql/)" -a "${1%_safe}" = 'mysqld' ]; then
+if [ ! -d "/var/lib/mysql/mysql/" ]; then
     rm -rf /var/lib/mysql/*
     chown -R mysql /var/lib/mysql && chgrp -R mysql /var/lib/mysql
-    mysql_install_db
-    /usr/bin/mysqld_safe
-    /usr/local/bin/mysql_secure.sh '0KmF5zArK5'
-	mysqladmin shutdown -u root -p"0KmF5zArK5"
+    mysql_install_db >> /dev/null
+    service mysql start >> /dev/null
+    /usr/local/bin/mysql_secure.sh 0KmF5zArK5 >> /dev/null
+	mysqladmin -uroot -p0KmF5zArK5 shutdown
 fi
 
-service php5-fpm start 
-service mysql start 
-service nginx start
+service php5-fpm start >> /dev/null
+service mysql start >> /dev/null
+service nginx start >> /dev/null
 exec /usr/sbin/sshd -D
